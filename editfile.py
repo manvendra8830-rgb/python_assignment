31c0137aa92876b29818afe0541d08f5e40e6203
manvendra8830-rgb <manvendra.8830@gmail.com>
2026-08-14 20:37:51 +0530

    Add files via upload

---
+++ editfile.py
@@ -0,0 +1,15 @@
+
+# fiel exccecution program ...
+
+file = open("myfile.txt",'w')
+
+file.write("Hello i am manvendra singh \n")
+file.write("this is file of content \n")
+file.write("I become a devops engneer ")
+
+file.close()
+
+print("sucessfully edit txt file.")
+
+
+
